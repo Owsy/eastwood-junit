a7ea30e4a3ece0dadb45cb3e1e96696cc52643ab
Rhodri Pugh <rod@pu-gh.com>
2015-05-02 08:17:38 +0100

    fix for junit format, one failure per testcase

--- src/owsy/eastwood_junit/core.clj
+++ src/owsy/eastwood_junit/core.clj
@@ -1,37 +1,33 @@
 
 (ns owsy.eastwood-junit.core
-  (:require [clojure.data.xml :refer [emit-str sexp-as-element]]
+  (:require [clojure.data.xml :refer [sexp-as-element]]
             [eastwood.lint :refer [lint]]))
 
-(defn by-ns
-  [acc warning]
-  (let [ns-key (-> warning :namespace-sym keyword)
-        info {:type (:linter warning)
-              :message (:msg warning)}]
-    (if (ns-key acc)
-      (update-in acc [ns-key] conj info)
-      (merge acc {ns-key [info]}))))
+(defn- to-testcase
+  [{:keys [column linter line msg] :as warning}]
+  (let [nss (:namespace-sym warning)]
+    [:testcase {:classname nss
+                :name (format "%s_line-%d_column-%d"
+                              (name nss)
+                              line
+                              column)}
+     [:failure {:type linter
+                :message msg}]]))
 
-(defn to-failure
-  [warning]
-  [:failure warning])
-
-(defn to-testcase
-  [ns-key warnings]
-  (apply
-    conj
-    [:testcase {:classname ns-key
-                :name ns-key}]
-    (vec (map to-failure warnings))))
+(defn- xml-for
+  [opts]
+  (let [warnings (:warnings (lint opts))
+        cases (map to-testcase warnings)]
+    (apply
+      conj
+      [:testsuite {:tests (count warnings)}]
+      (vec cases))))
 
 ;; Public
 ;; ------
 
 (defn element-for
   [opts]
-  (let [data (lint opts)
-        res (reduce by-ns {} (:warnings data))
-        cases (vec (map to-testcase (keys res) (vals res)))
-        xml (apply conj [:testsuite {:tests (count (keys res))}] cases)]
-    (sexp-as-element xml)))
+  (sexp-as-element
+    (xml-for opts)))
 
